0df4f470b97daf2d75c57a773b5a1505636caeb4
smallkun <smallerkun@foxmail.com>
2022-05-21 16:44:41 +0800

    master add 1

---
+++ src/main/java/com/example/springbootstudy/controller/MainController.java
@@ -0,0 +1,11 @@
+package com.example.springbootstudy.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author luck
+ * @create 2022-05-21 4:44 PM
+ */
+@Controller
+public class MainController {
+}
